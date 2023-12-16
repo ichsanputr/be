bc569b50cce6e1576580ed154075c376d9dd4ebe
Muhammad Ichsanul Fadhil <ichsanfadhil67@gmail.com>
2023-12-16 18:51:37 +0700

    a

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,15 @@ import { Knex } from 'knex';
 import { InjectConnection } from 'nest-knexjs';
 import { JwtService } from '@nestjs/jwt';
 import { v4 as uuid } from '@lukeed/uuid';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     @InjectConnection() private readonly knex: Knex,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private readonly http: HttpService
   ) { }
 
   @Post('/login')
@@ -41,6 +44,7 @@ export class AuthController {
         password: body.password,
         package: body.package,
         hit_request: 0,
+        google_id: '',
         access_token: uuid() + randInt.toString()
       });
 
@@ -50,6 +54,96 @@ export class AuthController {
 
     return {
       message: "Success"
+    };
+  }
+
+  @Post('/google/register')
+  async googleRegister(@Body() body: { code: String }) {
+    const data = await firstValueFrom(
+      this.http.post('https://oauth2.googleapis.com/token', {
+        code: body.code,
+        client_id: '676048652820-4mo55iv0sh18eglm89q1f4vpp471jk91.apps.googleusercontent.com',
+        client_secret: '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',
+        redirect_uri: 'postmessage',
+        grant_type: 'authorization_code'
+      }).pipe(
+        catchError(() => {
+          throw 'An error happened!';
+        }),
+      )
+    );
+
+    const resp = await firstValueFrom(
+      this.http.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          Authorization: `Bearer ${data.data.access_token}`
+        }
+      }).pipe(
+        catchError((err) => {
+          console.log(err)
+          throw 'An error happened!';
+        }),
+      )
+    );
+
+    const randInt = Math.floor(Math.random() * 1078) + 1
+
+    await this.knex.table('users')
+      .insert({
+        id: uuid(),
+        username: resp.data.name,
+        password: 1,
+        package: 'free',
+        hit_request: 0,
+        google_id: resp.data.sub.toString(),
+        access_token: uuid() + randInt.toString()
+      });
+
+    return {
+      message: "Success",
+    };
+  }
+
+  @Post('/google/verify')
+  async googleVerify(@Body() body: { code: String }) {
+    const data = await firstValueFrom(
+      this.http.post('https://oauth2.googleapis.com/token', {
+        code: body.code,
+        client_id: '676048652820-4mo55iv0sh18eglm89q1f4vpp471jk91.apps.googleusercontent.com',
+        client_secret: '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',
+        redirect_uri: 'postmessage',
+        grant_type: 'authorization_code'
+      }).pipe(
+        catchError(() => {
+          throw 'An error happened!';
+        }),
+      )
+    );
+
+    const resp = await firstValueFrom(
+      this.http.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          Authorization: `Bearer ${data.data.access_token}`
+        }
+      }).pipe(
+        catchError((err) => {
+          console.log(err)
+          throw 'An error happened!';
+        }),
+      )
+    );
+
+    const user = await this.knex.table('users')
+      .where('username', resp.data.name)
+      .where('google_id', resp.data.sub)
+      .first();
+
+    if (!user) {
+      throw new UnauthorizedException('Wrong authentication')
+    }
+
+    return {
+      message: "Success",
     };
   }
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-    imports: [JwtModule.register({ secret: 'hard!to-guess_secret' })],
+    imports: [JwtModule.register({ secret: 'hard!to-guess_secret' }), HttpModule],
     controllers: [AuthController]
 })
 export class AuthModule {}
